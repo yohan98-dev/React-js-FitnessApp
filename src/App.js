76818e7101221fec7373ea3bb02842b48455bb18
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-02-08 12:16:51 +0530

    Basic layout setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 export default App;
